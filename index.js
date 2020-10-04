b4f556687b524da9da5c54e2f9c1f8c484ec663a
TheBigSteph <nzali.stephane86@gmail.com>
2020-10-04 22:32:27 +0100

    Adding Configuration

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 var http = require('http');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./config');
 
 // The Server should respond to all requests with a string
 var server = http.createServer(function(req, res) {
@@ -72,9 +73,9 @@ var server = http.createServer(function(req, res) {
 
 });
 
-// Start the server, and have it listen on port 3000
-server.listen(3000, function() {
-    console.log('The server is listerning on port 3000 now');
+// Start the server
+server.listen(config.port, function() {
+    console.log('The server is listerning on port '+config.port+' in '+config.envName+' mode');
 });
 
 // Define the handlers
