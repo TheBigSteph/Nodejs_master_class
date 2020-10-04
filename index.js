44b2e28366eaf53ba4ef8a6aca6a685ba0e20ae0
TheBigSteph <nzali.stephane86@gmail.com>
2020-10-04 21:38:12 +0100

    Routing Requests

--- index.js
+++ index.js
@@ -36,11 +36,37 @@ var server = http.createServer(function(req, res) {
     req.on('end', function() {
         buffer += decoder.end();
 
-        // Send the response
-        res.end('Hello World!\n');
+        // Choose the handler this request should go to. If one is not found, use the notFound handler
+         var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-        // Log the request path
-        console.log('Request received with these payload:', buffer);
+         // Contruct the data object to send to the handler
+        var data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        }
+
+        // Route the request to the hanler specified in the router
+        chosenHandler(data, function(statusCode, payload) {
+            // Use the status code called back by the handler, or default to 200
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+            // Use the payload called back by the handler, or default to an empty object
+            payload = typeof(payload) == 'object' ? payload : {};
+
+            // Convert the payload to a string
+            var payloadString  = JSON.stringify(payload);
+
+            // Return the response
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            // Log the request path
+            console.log('Returning this response: ', statusCode, payloadString);
+        }); 
+
     });
 
 });
@@ -49,3 +75,22 @@ var server = http.createServer(function(req, res) {
 server.listen(3000, function() {
     console.log('The server is listerning on port 3000 now');
 });
+
+// Define the handlers
+var handlers = {}
+
+// Sample handler
+handlers.sample = function(data, callback) {
+    // callback a http status code, and a payload object
+    callback(406, {name: 'Sample handler'});
+}
+
+// Not found handler
+handlers.notFound = function(data, callback) {
+    callback(404);
+}
+
+// Define a resquest router
+var router = {
+    sample: handlers.sample
+};
