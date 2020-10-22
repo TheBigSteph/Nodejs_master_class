aee5a980e685d88d62ae6c5c933727f60af1bb21
TheBigSteph <nzali.stephane86@gmail.com>
2020-10-22 06:31:27 +0100

    tokens service: create a new token

--- index.js
+++ index.js
@@ -103,5 +103,6 @@ var unifiedServer = (req, res) => {
 // Define a resquest router
 const router = {
     ping: handlers.ping,
-    users: handlers.users
+    users: handlers.users,
+    tokens: handlers.tokens
 };
